564dc16673bb7fd92610198c7530de2a656d367b
jinhyeok409 <74577773+jinhyeok409@users.noreply.github.com>
2023-11-16 17:57:16 +0900

    session 7
    ~7

--- assignment/jinhyeok/board/src/main/java/com/study/board/controller/BoardController.java
+++ assignment/jinhyeok/board/src/main/java/com/study/board/controller/BoardController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main(){
-        return "Hello World";
+    @GetMapping("/board/write")
+    public String boardWriteForm() {
+
+        return "boardwrite";
     }
+
+
 }
