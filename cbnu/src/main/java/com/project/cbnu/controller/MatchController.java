15ddce6872b670cbd72822b2c5b3ef8278543c6c
KimMunho <74893339+KimMunho@users.noreply.github.com>
2023-11-30 07:58:44 +0900

    온도 둘째자리까지만 출력으로 수정 matching table에 신청자 누적으로 받기

--- cbnu/src/main/java/com/project/cbnu/controller/MatchController.java
+++ cbnu/src/main/java/com/project/cbnu/controller/MatchController.java
@@ -17,11 +17,6 @@ public class MatchController {
     //생성자 주입
     private final MatchService matchService;
 
-
-
-
-
-
     //회원가입 페이지 출력 요청
     @GetMapping ("/match/match1")
     public String match1Form() {
@@ -29,9 +24,8 @@ public class MatchController {
     }
 
     @PostMapping("/match/match1")
-    public String match1(@ModelAttribute MatchDTO matchDTO, @ModelAttribute MemberDTO memberDTO,HttpSession session, Model model) {
-        // 받은 데이터를 데이터베이스에 추가하는 로직을 수행합니다.
-        // 이 예시에서는 간단하게 로그로 출력합니다.
+    public String match1(@ModelAttribute MatchDTO matchDTO, @ModelAttribute MemberDTO memberDTO,HttpSession session, Object object) {
+
 
         Object getPlayername = session.getAttribute("loginUserid");
         Object getPlayerlevel = session.getAttribute("loginUserlevel");
@@ -40,17 +34,17 @@ public class MatchController {
         matchDTO.setGamenum(1);
         matchDTO.setPlayer((String) getPlayername);
         // (String)으로 강제 치환해서 matchDTO에 넣기;
-        matchDTO.setTeam("C");
+        matchDTO.setTeam("non");
         matchDTO.setPlayerlevel((Integer) getPlayerlevel);
         matchDTO.setPlayvoted(0);
 
-        System.out.println((String) getPlayername);
-        System.out.println((Integer) getPlayerlevel);
-
-
         matchService.save(matchDTO);
 
-        // 클라이언트에 응답을 보냅니다. (예시로 "Success" 문자열을 보냅니다.)
+
+
+
         return "main";
+
+
     }
 }
