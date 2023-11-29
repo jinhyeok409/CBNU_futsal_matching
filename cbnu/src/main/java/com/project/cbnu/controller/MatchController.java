3e397c54bde04970cf5ae39052192685feaaba73
jinhyeok409 <74577773+jinhyeok409@users.noreply.github.com>
2023-11-29 21:18:13 +0900

    123
    213

--- cbnu/src/main/java/com/project/cbnu/controller/MatchController.java
+++ cbnu/src/main/java/com/project/cbnu/controller/MatchController.java
@@ -34,16 +34,18 @@ public class MatchController {
         // 이 예시에서는 간단하게 로그로 출력합니다.
 
         Object getPlayername = session.getAttribute("loginUserid");
+        Object getPlayerlevel = session.getAttribute("loginUserlevel");
         // 객체선언 해서 플레이어 네임을 getPlayername에 변수를 지정하여 넣음
 
         matchDTO.setGamenum(1);
         matchDTO.setPlayer((String) getPlayername);
         // (String)으로 강제 치환해서 matchDTO에 넣기;
         matchDTO.setTeam("C");
-        matchDTO.setPlayerlevel(memberDTO.getUserlevel());
+        matchDTO.setPlayerlevel((Integer) getPlayerlevel);
         matchDTO.setPlayvoted(0);
 
         System.out.println((String) getPlayername);
+        System.out.println((Integer) getPlayerlevel);
 
 
         matchService.save(matchDTO);
